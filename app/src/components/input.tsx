b1f68aee95f93a0589142302fcaac456c0d6ad48
Avishek <132156247+newcould@users.noreply.github.com>
2023-05-04 14:46:24 +0000

    testing

--- app/src/components/input.tsx
+++ app/src/components/input.tsx
@@ -1,21 +1,21 @@
-import styled from "@emotion/styled";
-import { Button, ActionIcon, Textarea, Loader, Popover } from "@mantine/core";
-import { getHotkeyHandler, useHotkeys, useMediaQuery } from "@mantine/hooks";
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { FormattedMessage, useIntl } from "react-intl";
-import { useLocation, useNavigate } from "react-router-dom";
-import { useAppContext } from "../core/context";
-import { useAppDispatch, useAppSelector } from "../store";
-import { selectMessage, setMessage } from "../store/message";
-import { selectSettingsTab, openOpenAIApiKeyPanel } from "../store/settings-ui";
+import styled from '@emotion/styled';
+import {Button, ActionIcon, Textarea, Loader, Popover} from '@mantine/core';
+import {getHotkeyHandler, useHotkeys, useMediaQuery} from '@mantine/hooks';
+import {useCallback, useEffect, useMemo, useState} from 'react';
+import {FormattedMessage, useIntl} from 'react-intl';
+import {useLocation, useNavigate} from 'react-router-dom';
+import {useAppContext} from '../core/context';
+import {useAppDispatch, useAppSelector} from '../store';
+import {selectMessage, setMessage} from '../store/message';
+import {selectSettingsTab, openOpenAIApiKeyPanel} from '../store/settings-ui';
 import {
   speechRecognition,
   supportsSpeechRecognition,
-} from "../core/speech-recognition-types";
-import { useWhisper } from "@chengsokdara/use-whisper";
-import QuickSettings from "./quick-settings";
-import { useOption } from "../core/options/use-option";
-import { log } from "console";
+} from '../core/speech-recognition-types';
+import {useWhisper} from '@chengsokdara/use-whisper';
+import QuickSettings from './quick-settings';
+import {useOption} from '../core/options/use-option';
+import {log} from 'console';
 
 const Container = styled.div`
   background: #292933;
@@ -45,19 +45,18 @@ export default function MessageInput(props: MessageInputProps) {
   const message = useAppSelector(selectMessage);
   const [recording, setRecording] = useState(false);
   const [speechError, setSpeechError] = useState<string | null>(null);
-  const hasVerticalSpace = useMediaQuery("(min-height: 1000px)");
+  const hasVerticalSpace = useMediaQuery('(min-height: 1000px)');
   const [useOpenAIWhisper] = useOption<boolean>(
-    "speech-recognition",
-    "use-whisper"
+    'speech-recognition',
+    'use-whisper',
   );
-  const [openAIApiKey] = useOption<string>("openai", "apiKey");
+  const [openAIApiKey] = useOption<string>('openai', 'apiKey');
 
-  const [initialMessage, setInitialMessage] = useState("");
-  const { transcribing, transcript, startRecording, stopRecording } =
-    useWhisper({
-      apiKey: openAIApiKey || " ",
-      streaming: false,
-    });
+  const [initialMessage, setInitialMessage] = useState('');
+  const {transcribing, transcript, startRecording, stopRecording} = useWhisper({
+    apiKey: openAIApiKey || ' ',
+    streaming: false,
+  });
 
   const navigate = useNavigate();
   const context = useAppContext();
@@ -67,16 +66,16 @@ export default function MessageInput(props: MessageInputProps) {
   const tab = useAppSelector(selectSettingsTab);
 
   const [showMicrophoneButton] = useOption<boolean>(
-    "speech-recognition",
-    "show-microphone"
+    'speech-recognition',
+    'show-microphone',
   );
-  const [submitOnEnter] = useOption<boolean>("input", "submit-on-enter");
+  const [submitOnEnter] = useOption<boolean>('input', 'submit-on-enter');
 
   const onChange = useCallback(
     (e: React.ChangeEvent<HTMLTextAreaElement>) => {
       dispatch(setMessage(e.target.value));
     },
-    [dispatch]
+    [dispatch],
   );
 
   const pathname = useLocation().pathname;
@@ -90,9 +89,9 @@ export default function MessageInput(props: MessageInputProps) {
 
     if (id) {
       if (!window.location.pathname.includes(id)) {
-        navigate("/chat/" + id);
+        navigate('/chat/' + id);
       }
-      dispatch(setMessage(""));
+      dispatch(setMessage(''));
     }
 
     setTimeout(function () {
@@ -104,7 +103,7 @@ export default function MessageInput(props: MessageInputProps) {
 
   const onSpeechError = useCallback(
     (e: any) => {
-      console.error("speech recognition error", e);
+      console.error('speech recognition error', e);
       setSpeechError(e.message);
 
       try {
@@ -117,23 +116,23 @@ export default function MessageInput(props: MessageInputProps) {
 
       setRecording(false);
     },
-    [stopRecording]
+    [stopRecording],
   );
 
   const onHideSpeechError = useCallback(() => setSpeechError(null), []);
   const onSpeechStart1 = useCallback(async () => {
-    console.log("onSpeechStart1");
+    console.log('onSpeechStart1');
 
     let granted = true;
     let denied = false;
 
     try {
       const result = await navigator.permissions.query({
-        name: "microphone" as any,
+        name: 'microphone' as any,
       });
-      if (result.state == "granted") {
+      if (result.state == 'granted') {
         granted = true;
-      } else if (result.state == "denied") {
+      } else if (result.state == 'denied') {
         denied = true;
       }
     } catch (e) {}
@@ -144,7 +143,7 @@ export default function MessageInput(props: MessageInputProps) {
           video: false,
           audio: true,
         });
-        stream.getTracks().forEach((track) => track.stop());
+        stream.getTracks().forEach(track => track.stop());
         granted = true;
       } catch (e) {
         denied = true;
@@ -152,13 +151,13 @@ export default function MessageInput(props: MessageInputProps) {
     }
 
     if (denied) {
-      onSpeechError(new Error("speech permission was not granted"));
+      onSpeechError(new Error('speech permission was not granted'));
       return;
     }
 
     try {
       if (!recording) {
-        console.log("recor");
+        console.log('recor');
 
         setRecording(true);
 
@@ -176,19 +175,19 @@ export default function MessageInput(props: MessageInputProps) {
           speechRecognition.continuous = true;
           speechRecognition.interimResults = true;
 
-          speechRecognition.onresult = (event) => {
-            let transcript = "";
+          speechRecognition.onresult = event => {
+            let transcript = '';
             for (let i = 0; i < event.results.length; i++) {
               if (event.results[i].isFinal && event.results[i][0].confidence) {
                 transcript += event.results[i][0].transcript;
               }
             }
-            dispatch(setMessage(initialMessage + " " + transcript));
+            dispatch(setMessage(initialMessage + ' ' + transcript));
           };
 
           speechRecognition.start();
         } else {
-          onSpeechError(new Error("not supported"));
+          onSpeechError(new Error('not supported'));
         }
       } else {
         if (useOpenAIWhisper || !supportsSpeechRecognition) {
@@ -198,7 +197,7 @@ export default function MessageInput(props: MessageInputProps) {
           speechRecognition.stop();
           setRecording(false);
         } else {
-          onSpeechError(new Error("not supported"));
+          onSpeechError(new Error('not supported'));
         }
       }
     } catch (e) {
@@ -206,18 +205,18 @@ export default function MessageInput(props: MessageInputProps) {
     }
   }, []);
   const onSpeechStart = useCallback(async () => {
-    console.log("onSpeechStart");
+    console.log('onSpeechStart');
 
     let granted = false;
     let denied = false;
 
     try {
       const result = await navigator.permissions.query({
-        name: "microphone" as any,
+        name: 'microphone' as any,
       });
-      if (result.state == "granted") {
+      if (result.state == 'granted') {
         granted = true;
-      } else if (result.state == "denied") {
+      } else if (result.state == 'denied') {
         denied = true;
       }
     } catch (e) {}
@@ -228,7 +227,7 @@ export default function MessageInput(props: MessageInputProps) {
           video: false,
           audio: true,
         });
-        stream.getTracks().forEach((track) => track.stop());
+        stream.getTracks().forEach(track => track.stop());
         granted = true;
       } catch (e) {
         denied = true;
@@ -236,13 +235,13 @@ export default function MessageInput(props: MessageInputProps) {
     }
 
     if (denied) {
-      onSpeechError(new Error("speech permission was not granted"));
+      onSpeechError(new Error('speech permission was not granted'));
       return;
     }
 
     try {
       if (!recording) {
-        console.log("recor");
+        console.log('recor');
 
         setRecording(true);
 
@@ -260,19 +259,19 @@ export default function MessageInput(props: MessageInputProps) {
           speechRecognition.continuous = true;
           speechRecognition.interimResults = true;
 
-          speechRecognition.onresult = (event) => {
-            let transcript = "";
+          speechRecognition.onresult = event => {
+            let transcript = '';
             for (let i = 0; i < event.results.length; i++) {
               if (event.results[i].isFinal && event.results[i][0].confidence) {
                 transcript += event.results[i][0].transcript;
               }
             }
-            dispatch(setMessage(initialMessage + " " + transcript));
+            dispatch(setMessage(initialMessage + ' ' + transcript));
           };
 
           speechRecognition.start();
         } else {
-          onSpeechError(new Error("not supported"));
+          onSpeechError(new Error('not supported'));
         }
       } else {
         if (useOpenAIWhisper || !supportsSpeechRecognition) {
@@ -282,7 +281,7 @@ export default function MessageInput(props: MessageInputProps) {
           speechRecognition.stop();
           setRecording(false);
         } else {
-          onSpeechError(new Error("not supported"));
+          onSpeechError(new Error('not supported'));
         }
       }
     } catch (e) {
@@ -300,7 +299,7 @@ export default function MessageInput(props: MessageInputProps) {
   useEffect(() => {
     if (useOpenAIWhisper || !supportsSpeechRecognition) {
       if (!transcribing && !recording && transcript?.text) {
-        dispatch(setMessage(initialMessage + " " + transcript.text));
+        dispatch(setMessage(initialMessage + ' ' + transcript.text));
       }
     }
   }, [
@@ -314,26 +313,26 @@ export default function MessageInput(props: MessageInputProps) {
 
   useHotkeys([
     [
-      "n",
+      'n',
       () =>
-        document.querySelector<HTMLTextAreaElement>("#message-input")?.focus(),
+        document.querySelector<HTMLTextAreaElement>('#message-input')?.focus(),
     ],
   ]);
 
   const blur = useCallback(() => {
-    document.querySelector<HTMLTextAreaElement>("#message-input")?.blur();
+    document.querySelector<HTMLTextAreaElement>('#message-input')?.blur();
   }, []);
 
   const rightSection = useMemo(() => {
     return (
       <div
         style={{
-          opacity: "0.8",
-          paddingRight: "0.5rem",
-          display: "flex",
-          justifyContent: "flex-end",
-          alignItems: "center",
-          width: "100%",
+          opacity: '0.8',
+          paddingRight: '0.5rem',
+          display: 'flex',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+          width: '100%',
         }}
       >
         {context.generating && (
@@ -345,16 +344,16 @@ export default function MessageInput(props: MessageInputProps) {
               onClick={() => {
                 context.chat.cancelReply(
                   context.currentChat.chat?.id,
-                  context.currentChat.leaf!.id
+                  context.currentChat.leaf!.id,
                 );
               }}
             >
               <FormattedMessage
-                defaultMessage={"Cancel"}
+                defaultMessage={'Cancel'}
                 description="Label for the button that can be clicked while the AI is generating a response to cancel generation"
               />
             </Button>
-            <Loader size="xs" style={{ padding: "0 0.8rem 0 0.5rem" }} />
+            <Loader size="xs" style={{padding: '0 0.8rem 0 0.5rem'}} />
           </>
         )}
         {!context.generating && (
@@ -374,8 +373,8 @@ export default function MessageInput(props: MessageInputProps) {
                       <i
                         className="fa fa-microphone"
                         style={{
-                          fontSize: "90%",
-                          color: recording ? "red" : "inherit",
+                          fontSize: '90%',
+                          color: recording ? 'red' : 'inherit',
                         }}
                       />
                     )}
@@ -384,17 +383,17 @@ export default function MessageInput(props: MessageInputProps) {
                 <Popover.Dropdown>
                   <div
                     style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      alignItems: "flex-start",
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'flex-start',
                     }}
                   >
                     <p
                       style={{
                         fontFamily: `"Work Sans", sans-serif`,
-                        fontSize: "0.9rem",
-                        textAlign: "center",
-                        marginBottom: "0.5rem",
+                        fontSize: '0.9rem',
+                        textAlign: 'center',
+                        marginBottom: '0.5rem',
                       }}
                     >
                       Sorry, an error occured trying to record audio.
@@ -412,7 +411,7 @@ export default function MessageInput(props: MessageInputProps) {
               </Popover>
             )}
             <ActionIcon size="xl" onClick={onSubmit}>
-              <i className="fa fa-paper-plane" style={{ fontSize: "90%" }} />
+              <i className="fa fa-paper-plane" style={{fontSize: '90%'}} />
             </ActionIcon>
           </>
         )}
@@ -432,18 +431,18 @@ export default function MessageInput(props: MessageInputProps) {
 
   const disabled = context.generating;
 
-  const isLandingPage = pathname === "/";
+  const isLandingPage = pathname === '/';
   if (context.isShare || (!isLandingPage && !context.id)) {
     return null;
   }
 
   const hotkeyHandler = useMemo(() => {
     const keys = [
-      ["Escape", blur, { preventDefault: true }],
-      ["ctrl+Enter", onSubmit, { preventDefault: true }],
+      ['Escape', blur, {preventDefault: true}],
+      ['ctrl+Enter', onSubmit, {preventDefault: true}],
     ];
     if (submitOnEnter) {
-      keys.unshift(["Enter", onSubmit, { preventDefault: true }]);
+      keys.unshift(['Enter', onSubmit, {preventDefault: true}]);
     }
     const handler = getHotkeyHandler(keys as any);
     return handler;
@@ -459,7 +458,7 @@ export default function MessageInput(props: MessageInputProps) {
           minRows={hasVerticalSpace || context.isHome ? 3 : 2}
           maxRows={12}
           placeholder={intl.formatMessage({
-            defaultMessage: "Enter a message here...",
+            defaultMessage: 'Enter a message here...',
           })}
           value={message}
           onChange={onChange}
